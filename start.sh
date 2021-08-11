2670116cab0f06b9308f432e3df5d8e7d855dafb
yayanksenpai <84301128+yayanksenpai@users.noreply.github.com>
2021-08-12 06:54:23 +0700

    External load

--- start.sh
+++ start.sh
@@ -1,1 +1,15 @@
+if [[ -n $CONFIG_URL ]]; then
+	wget -q $CONFIG_URL -O /usr/src/app/config.env
+fi
+
+if [[ -n $TOKEN_PICKLE_URL ]]; then
+	wget -q $TOKEN_PICKLE_URL -O /usr/src/app/token.pickle
+fi
+
+if [[ -n $ACCOUNTS_ZIP_URL ]]; then
+	wget -q $ACCOUNTS_ZIP_URL -O /usr/src/app/accounts.zip
+	unzip accounts.zip -d /usr/src/app/accounts
+	rm accounts.zip
+fi
+
 ./aria.sh; python3 -m bot
